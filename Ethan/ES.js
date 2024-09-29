bd20c359ea76760b434dc27af49764db1ffce535
LelouchZeroLamperouge <157536738+LelouchZeroLamperouge@users.noreply.github.com>
2024-09-29 18:47:28 -0500

    Add files via upload

---
+++ Ethan/ES.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const $ = selector => document.querySelector(selector);
+
+function filterEventsES() {
+    const input = document.getElementById('SearchBar');
+    const filter = input.value.toLowerCase();
+    const table = document.getElementById('ET');
+    const rows = table.getElementsByTagName('tr');
+
+    for (let i = 1; i < rows.length; i++) { 
+        const cells = rows[i].getElementsByTagName('td')[0].textContent.toLowerCase();
+        if (!cells.includes(filter)) {
+            rows[i].style.display = 'none';
+        } else {
+            rows[i].style.display = '';
+        }
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    $("#Search").addEventListener("click", () => {
+        filterEventsES();
+    });
+});
